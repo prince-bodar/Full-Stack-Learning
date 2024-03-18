bb5b478b0b349e2ce1732bd1109d3d184e5b174f
prince-bodar <bodarprins4@gmail.com>
2024-03-18 11:34:47 +0530

    18-03-2024

--- JAVASCRIPT/6_opertor.js
+++ JAVASCRIPT/6_opertor.js
@@ -6,8 +6,9 @@ let a =10 , b=5;
 
 console.log("a + b  = ",a+b);
 console.log("a - b  = ",a-b);
-console.log("a + b = ",++a);
+console.log("a + b  = ",++a);
 console.log("a / b  = ",a/b);
+console.log("a % b  = ",a%b);
 console.log("a ** b = ",a**b);
 console.log("++a    = ",++a);
 console.log("a++    = ",a++);
