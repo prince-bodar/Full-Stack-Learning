605eeb52ce65b71c79ab35a8a9617e464bab20aa
prince-bodar <bodarprins4@gmail.com>
2024-05-08 10:41:33 +0530

    08-05-2024

---
+++ JQUERY/1/1.js
@@ -0,0 +1,23 @@
+// $(function(){
+//     $('#button').click(function(){
+//         $('#heading').hide().slide
+//     })
+// })
+
+// $(function(){
+//     $('#button1').click(function(){
+//         $('#heading').show().slideDown(2000)
+//     })
+// })
+
+// $(function(){
+//     $("#button").click(function(){
+//       $("#heading").slideUp("slow");
+//     });
+//   });
+  
+// $(function(){
+//     $("#button1").click(function(){
+//        $("#heading").slideDown("slow");
+//     })
+// })
